754d30c748dec0d81d002d1a22c406f5fd9a5a17
Brent <brentblitek26@gmail.com>
2022-04-06 08:04:23 -0500

    added class to leaderboard vc

---
+++ FinalProject/LeaderboardViewController.swift
@@ -0,0 +1,17 @@
+//  LeaderboardViewController.swift
+//  FinalProject
+//  Created by BRENT BLITEK on 4/6/22.
+
+import UIKit
+
+class LeaderboardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        
+    }
+    
+    
+    }
