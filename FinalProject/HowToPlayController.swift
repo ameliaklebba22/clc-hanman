6507b8889971e31abed35755d4daca4b5fa8ff32
Amelia <amelia.klebba@gmail.com>
2022-04-06 07:35:01 -0500

    a bit of ui stuff

--- FinalProject/HowToPlayController.swift
+++ FinalProject/HowToPlayController.swift
@@ -3,10 +3,7 @@ class HowToPlayController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let x = CAGradientLayer()
-        x.frame = self.view.bounds
-        x.colors = [UIColor.cyan.cgColor, UIColor.systemYellow.cgColor]
-        self.view.layer.insertSublayer(x, at: 0)
+    
     }
    
 
