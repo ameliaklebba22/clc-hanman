0b762e3e41e5933f8fd18053ac6e7f6de793e6a7
Brent <brentblitek26@gmail.com>
2022-04-20 08:08:26 -0500

    personal leaderboard changes

--- FinalProject/PersonalViewController.swift
+++ FinalProject/PersonalViewController.swift
@@ -1,14 +1,33 @@
 
 import UIKit
 
-class PersonalViewController: UIViewController {
+class PersonalViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    
+    
+    
+    @IBOutlet weak var personalTableViewOutlet: UITableView!
     var userHighScore : [PlayerScore] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        personalTableViewOutlet.delegate = self
+        personalTableViewOutlet.dataSource = self
+        
     }
     
+    override func viewDidAppear(_ animated: Bool) {
 
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+       return userHighScore.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
  
 }
